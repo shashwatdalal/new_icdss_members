dfed6e5a133bc793f006f80bb705a25801c5291f
Shashwat Dalal <spd16@ic.ac.uk>
2019-09-27 17:18:14 +0100

    Update get_members.py

--- get_members.py
+++ get_members.py
@@ -1,13 +1,15 @@
+import codecs
+import json
 import os
 import sys
 
 import requests
-import json
 
 # get api-key 
 token = sys.argv[1]
 headers = {"X-API-Key": token}
 
+# Imperial Union API end-point
 END_POINT = 'https://eactivities.union.ic.ac.uk/API/CSP'
 
 # get society code
@@ -18,5 +20,5 @@ society_code = json.loads(response.text)[0]['Code']
 year = '19-20'
 members_url = os.path.join(END_POINT, society_code, 'reports', 'members?year={}'.join(year))
 response = requests.get(members_url, headers=headers)
-print(response.encoding)
-print(json.loads(response.text))
+decoded_text = codecs.decode(response.text, 'utf-8-sig')
+print(json.loads(decoded_text))
