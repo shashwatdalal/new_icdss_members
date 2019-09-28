8584987dbc1afb3ddfb90e5dde54124bef3f6989
Shashwat Dalal <spd16@ic.ac.uk>
2019-09-28 12:34:26 +0100

    Update get_members.py

--- get_members.py
+++ get_members.py
@@ -1,57 +1,81 @@
 import codecs
+from datetime import datetime
 import json
 import os
 import sys
 
 import requests
 
-def _get_s3_cids(s3_json):
-	old_cids = set()
+
+def _get_s3_cids():
+	# get list of Member's CID from s3
+	
+	with open('members.json') as f:
+		s3_json = json.load(f)
+	s3_cids = set()
 	for period in s3_json['growth']:
 		for cid in period['new_members']:
-			old_cids.add(cid)
-	return old_cids
+			s3_cids.add(cid)
+	return s3_json, s3_cids
 
-# get api-keys 
-token = sys.argv[1]
-headers = {"X-API-Key": token}
+def _get_union_cids():
+	# get list of Members's CID from Union API
+	
+	# get api-keys 
+	token = sys.argv[1]
+	headers = {"X-API-Key": token}
 
-# Imperial Union API end-point
-END_POINT = 'https://eactivities.union.ic.ac.uk/API/CSP'
+	# Imperial Union API end-point
+	END_POINT = 'https://eactivities.union.ic.ac.uk/API/CSP'
 
-# get society code
-response = requests.get(END_POINT, headers=headers)
-society_code = json.loads(response.text)[0]['Code']
+	# get society code
+	response = requests.get(END_POINT, headers=headers)
+	society_code = json.loads(response.text)[0]['Code']
 
-# get members from union api
-year = '19-20'
-members_url = os.path.join(END_POINT, society_code, 'reports', 'members?year={}'.format(year))
-response = requests.get(members_url, headers=headers)
-union_json = json.loads(response.text)
-union_cids = set()
-for member in union_json:
-	union_cids.add(member['CID'])
-print(union_cids)
+	# get members from union api
+	year = '19-20'
+	members_url = os.path.join(END_POINT, society_code, 'reports', 'members?year={}'.format(year))
+	response = requests.get(members_url, headers=headers)
+	union_json = json.loads(response.text)
+	union_cids = set()
+	for member in union_json:
+		union_cids.add(member['CID'])
+	return union_cids
 
-# get previous members
-with open('members.json') as f:
-	s3_json = json.load(f)
-old_cids = _get_s3_cids(s3_json)
-print(old_cids)
-
-# Get Slack URL
-slack_endpoint = sys.argv[2]
-
-# send update to slack channel
-message = {
-	"blocks": [
+if __name__ == "__main__":
+	# get previous member list
+	s3_json, s3_cids = _get_s3_cids()
+	# get updated member list
+	union_cids = _get_union_cids()
+	
+	# calculate new sign-ups
+	new_members = union_cids - s3_cids
+	print(new_members)
+	
+	# update members json
+	s3_json['growth'].append(
 		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": ":newspaper: *Number of signups*: {}".format(len(members))
-			}
+			'timestamp': str(datetime.now()),
+			'new_members': list(new_members)
 		}
-	]
-}
-response = requests.post(slack_endpoint, json=message)
+	)
+	
+	print(s3_json)
+	
+
+# # Get Slack URL
+# slack_endpoint = sys.argv[2]
+
+# # send update to slack channel
+# message = {
+# 	"blocks": [
+# 		{
+# 			"type": "section",
+# 			"text": {
+# 				"type": "mrkdwn",
+# 				"text": ":newspaper: *Number of signups*: {}".format(len(members))
+# 			}
+# 		}
+# 	]
+# }
+# response = requests.post(slack_endpoint, json=message)
